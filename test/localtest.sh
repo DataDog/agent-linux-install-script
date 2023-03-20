7d8005371afd7f92e1c8ad6965728555e20b240f
Pythyu <antoine.zellmeyer@datadoghq.com>
2023-03-20 13:49:55 +0100

    test(localtest): debug dpkg-query

--- test/localtest.sh
+++ test/localtest.sh
@@ -21,7 +21,7 @@ if command -v dpkg > /dev/null; then
     apt-get install -y debsums
     debsums -c ${EXPECTED_FLAVOR}
     INSTALLED_VERSION=$(dpkg-query -f='${source:Upstream-Version}' -W ${EXPECTED_FLAVOR})
-    echo "$(dpkg-query -f='${source:Upstream-Version}' -l)"
+    echo "${EXPECTED_FLAVOR}"
 else
     # skip verification of mode/user/group, because these are
     # changed by the postinstall scriptlet
