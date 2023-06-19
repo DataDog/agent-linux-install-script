8ae65ec8c9f754f133bf0e38127ef235cf10e74d
Marethyu <45374460+Pythyu@users.noreply.github.com>
2023-06-19 14:39:24 +0200

    SLES 11 install script support (#51)

--- test/dockertest.sh
+++ test/dockertest.sh
@@ -31,6 +31,9 @@ while [[ $# -gt 0 ]]; do
     --no-agent)
       DD_NO_AGENT_INSTALL="$2"
       ;;
+    --old-suse)
+      DD_OLD_SUSE="$2"
+      ;;
     -*|--*)
       echo "Unknown option $1"
       exit 1
@@ -41,6 +44,12 @@ done
 
 [ -z "$SCRIPT" ] && echo "Please provide script file to test via -s/--script" && exit 1;
 [ -z "$IMAGE" ] && echo "Please provide image to test via -i/--image" && exit 1;
+
+if [ -z "$DD_OLD_SUSE" ]; then
+    ENTRYPOINT_PATH="/tmp/vol/test/localtest.sh"
+else
+    ENTRYPOINT_PATH="/tmp/vol/test/old-suse-startup.sh"
+fi
 
 docker run --rm --platform $PLATFORM -v $(pwd):/tmp/vol \
   -e DD_SYSTEM_PROBE_ENSURE_CONFIG="${DD_SYSTEM_PROBE_ENSURE_CONFIG}" \
@@ -53,4 +62,5 @@ docker run --rm --platform $PLATFORM -v $(pwd):/tmp/vol \
   -e DD_APM_INSTRUMENTATION_ENABLED="${DD_APM_INSTRUMENTATION_ENABLED}" \
   -e DD_NO_AGENT_INSTALL="$DD_NO_AGENT_INSTALL" \
   -e DD_APM_INSTRUMENTATION_LANGUAGES="${DD_APM_INSTRUMENTATION_LANGUAGES}" \
-  --entrypoint /tmp/vol/test/localtest.sh "$IMAGE"
+  -e DD_OLD_SUSE="$DD_OLD_SUSE" \
+  --entrypoint "$ENTRYPOINT_PATH" "$IMAGE"
