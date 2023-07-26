97fe04c39d512668b07efc9c2939897693ff1516
Spencer Gilbert <spencer.gilbert@datadoghq.com>
2023-07-26 09:14:47 -0400

    chmod the OPW envfile for better security (#72)
    * chmod the OPW envfile for better security
    
    * Place chmod closer to file creation

--- install_script_op_worker1.sh
+++ install_script_op_worker1.sh
@@ -464,6 +464,8 @@ else
   printf "\033[34m\n* Creating $env_file for $worker_flavor.service.\n\033[0m\n"
   $sudo_cmd sh -c "touch $env_file"
 
+  $sudo_cmd chmod 640 "$env_file"
+
   if [ "$apikey" ]; then
     printf "\033[34m  * Assigning DD_API_KEY.\n\033[0m\n"
     $sudo_cmd sh -c "echo DD_API_KEY=$apikey >> $env_file"
