70847a23094c87df7abad06ed58d6c42712a5029
Marethyu <45374460+Pythyu@users.noreply.github.com>
2023-08-30 12:22:57 +0200

    [SLES11] migrating tests using third party dependency to internal image (#80)

--- test/dockertest.sh
+++ test/dockertest.sh
@@ -31,9 +31,6 @@ while [[ $# -gt 0 ]]; do
     --no-agent)
       DD_NO_AGENT_INSTALL="$2"
       ;;
-    --old-suse)
-      DD_OLD_SUSE="$2"
-      ;;
     --apt-url)
       TESTING_APT_URL="$2"
       ;;
@@ -63,9 +60,7 @@ done
 [ -z "$SCRIPT" ] && echo "Please provide script file to test via -s/--script" && exit 1;
 [ -z "$IMAGE" ] && echo "Please provide image to test via -i/--image" && exit 1;
 
-if [ "$DD_OLD_SUSE" == "true" ]; then
-    ENTRYPOINT_PATH="/tmp/vol/test/old-suse-startup.sh"
-elif [ "$DD_OPW" == "true" ]; then
+if [ "$DD_OPW" == "true" ]; then
     ENTRYPOINT_PATH="/tmp/vol/test/op-worker-test.sh"
 else
     ENTRYPOINT_PATH="/tmp/vol/test/localtest.sh"
@@ -82,7 +77,6 @@ docker run --rm --platform $PLATFORM -v $(pwd):/tmp/vol \
   -e DD_APM_INSTRUMENTATION_ENABLED="${DD_APM_INSTRUMENTATION_ENABLED}" \
   -e DD_NO_AGENT_INSTALL="$DD_NO_AGENT_INSTALL" \
   -e DD_APM_INSTRUMENTATION_LANGUAGES="${DD_APM_INSTRUMENTATION_LANGUAGES}" \
-  -e DD_OLD_SUSE="$DD_OLD_SUSE" \
   -e DD_OP_WORKER_MINOR_VERSION="${MINOR_VERSION}" \
   -e DD_OP_PIPELINE_ID=123 \
   -e DD_OPW_INSTALL_CLASSIC_AGENT=${DD_OPW_INSTALL_CLASSIC_AGENT}\
--- test/old-suse-startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-echo "VERSION:11" > /etc/SuSE-release
-
-zypper install -y which 
-zypper addrepo https://ftp5.gwdg.de/pub/opensuse/discontinued/distribution/11.1/repo/oss/ discontinued
-zypper install --force -y bash=3.2-141.16
-
-bash -c "/tmp/vol/test/localtest.sh"
