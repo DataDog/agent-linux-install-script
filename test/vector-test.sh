f93a951e12c4bd7f1442ccd44c305e56117b9fc4
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2023-10-26 13:49:01 +0200

    fix shellcheck warning

--- test/vector-test.sh
+++ test/vector-test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
 
-# The Vector install script only configures the repos, it doesn't install 
+# The Vector install script only configures the repos, it doesn't install
 
 $SCRIPT
 
-EXPECTED_FLAVOR=vector
 RESULT=0
 
 if command -v dpkg > /dev/null; then
