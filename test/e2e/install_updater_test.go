1ac3156b57135a2efd61ff1d429a39345cab8e25
Raphael Gavache <raphael.gavache@datadoghq.com>
2024-03-11 17:47:15 +0100

    Add option to install updater (#143)
    * install updater
    
    * Add test
    
    * add updater test
    
    * update
    
    * update
    
    * fix
    
    * fix
    
    * simplify

---
+++ test/e2e/install_updater_test.go
@@ -0,0 +1,49 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package e2e
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/e2e"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/e2e/params"
+)
+
+type installUpdaterTestSuite struct {
+	linuxInstallerTestSuite
+}
+
+func TestInstallUpdaterSuite(t *testing.T) {
+	if flavor != agentFlavorDatadogAgent {
+		t.Skip("updater test supports only datadog-agent flavor")
+	}
+	stackName := fmt.Sprintf("install-updater-%s-%s", flavor, platform)
+	t.Run(stackName, func(t *testing.T) {
+		testSuite := &installUpdaterTestSuite{}
+		e2e.Run(t,
+			testSuite,
+			e2e.EC2VMStackDef(getEC2Options(t)...),
+			params.WithStackName(stackName),
+		)
+	})
+}
+
+func (s *installUpdaterTestSuite) TestInstallUpdater() {
+	t := s.T()
+	vm := s.Env().VM
+	cmd := fmt.Sprintf("DD_INSTALL_UPDATER=true DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=%s DD_SITE=\"datadoghq.com\" DD_REPO_URL=datad0g.com DD_AGENT_DIST_CHANNEL=beta bash -c \"$(cat scripts/install_script_agent7.sh)\"", apiKey)
+	output := vm.Execute(cmd)
+	t.Log(output)
+
+	s.assertInstallScript()
+}
+
+func (s *installUpdaterTestSuite) assertInstallScript() {
+	t := s.T()
+	vm := s.Env().VM
+	assertFileExists(t, vm, "/lib/systemd/system/datadog-updater.service")
+}
