d0e24ab2754ce262bc356e3ec12b60830b99ee74
Arthur Bellal <bellal.arthur@gmail.com>
2024-05-22 11:05:35 +0200

    (fleet) filter out packages installed by the installer (#208)
    * (fleet) filter out packages installed by the installer
    
    This PR:
    - Moves the installation of the installer at the top after input resolution
    - Adds a way to check if a package is installed by the installer
    - Filter out packages installed by the installer when installing package manager packages
    - Avoid running the setup script of the injector if its installed with the installer
    
    * fix function
    
    * reorder logs
    
    * fix command used
    
    * fix array expension
    
    * add `function`
    
    * missing sudo command
    
    * add """"e2e"""" test
    
    * move install_installer to the right location
    
    * feedback
    
    * debug
    
    * missing telemetry entry
    
    * try -E
    
    * try without tee
    
    * cleanup env between tests
    
    * perms
    
    * another test attempt
    
    * fix apm injection scripts condition
    
    * typo fix
    
    * e2e fix attempt
    
    * better fix
    
    * missing sudo
    
    * add some debugging
    
    * debug
    
    * fix e2e on centos
    
    * fix typo

--- test/e2e/install_installer_test.go
+++ test/e2e/install_installer_test.go
@@ -37,9 +37,10 @@ func TestInstallUpdaterSuite(t *testing.T) {
 func (s *installUpdaterTestSuite) TestInstallUpdater() {
 	t := s.T()
 	vm := s.Env().VM
-	cmd := fmt.Sprintf("DD_INSTALLER=true DD_APM_INSTRUMENTATION_ENABLED=host DD_APM_INSTRUMENTATION_LANGUAGES=\" \" DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=%s DD_SITE=\"datadoghq.com\" DD_REPO_URL=datad0g.com DD_AGENT_DIST_CHANNEL=beta bash -c \"$(cat scripts/install_script_agent7.sh)\"", apiKey)
+	cmd := fmt.Sprintf("DD_INSTALLER=true DD_APM_INSTRUMENTATION_ENABLED=host DD_APM_INSTRUMENTATION_LANGUAGES=\" \" DD_API_KEY=%s DD_SITE=\"datadoghq.com\" bash -c \"$(cat scripts/install_script_agent7.sh)\"", apiKey)
 	output := vm.Execute(cmd)
 	t.Log(output)
+	defer s.purge()
 
 	s.assertInstallScript()
 	s.assertInstallerInstalled()
@@ -48,6 +49,47 @@ func (s *installUpdaterTestSuite) TestInstallUpdater() {
 	s.assertUninstallInstaller()
 	// agent should not be uninstalled
 	s.assertInstallScript()
+}
+
+// mock installer, it will return 0 for datadog-apm-inject and datadog-apm-library-python
+const isInstalledScript = `#!/bin/bash
+[[ "$1" == "is-installed" ]] && { [[ "$2" == "datadog-apm-inject" || "$2" == "datadog-apm-library-python" ]] && exit 0 || exit 1; } || { echo "Unsupported command"; exit 2; }`
+
+func (s *installUpdaterTestSuite) TestPackagesInstalledByInstallerAreNotInstalledByPackageManager() {
+	t := s.T()
+	vm := s.Env().VM
+	vm.Execute("echo 'export PATH=/usr/local/bin:$PATH' | sudo tee -a /etc/profile")
+	vm.Execute("echo '" + isInstalledScript + "' | sudo tee /usr/local/bin/datadog-installer && sudo chmod +x /usr/local/bin/datadog-installer")
+	_, _ = vm.ExecuteWithError("echo '" + isInstalledScript + "' | sudo tee /sbin/datadog-installer && sudo chmod +x /sbin/datadog-installer")
+	cmd := fmt.Sprintf("DD_INSTALLER=true DD_APM_INSTRUMENTATION_ENABLED=host DD_API_KEY=%s DD_SITE=\"datadoghq.com\" bash -c \"$(cat scripts/install_script_agent7.sh)\"", apiKey)
+	output := vm.Execute(cmd)
+	t.Log(output)
+	defer s.purge()
+
+	s.assertInstallScript()
+	s.assertPackageInstalledByPackageManager("datadog-agent")
+	s.assertPackageInstalledByPackageManager("datadog-apm-library-ruby")
+	s.assertPackageNotInstalledByPackageManager("datadog-apm-inject")
+	s.assertPackageNotInstalledByPackageManager("datadog-apm-library-python")
+}
+
+func (s *installUpdaterTestSuite) purge() {
+	t := s.T()
+	vm := s.Env().VM
+	t.Helper()
+	if _, err := vm.ExecuteWithError("command -v apt"); err == nil {
+		t.Log("Uninstall with apt")
+		// remove all datadog packages
+		vm.Execute("sudo apt remove -y --purge 'datadog-*'")
+	} else if _, err = vm.ExecuteWithError("command -v yum"); err == nil {
+		t.Log("Uninstall with yum")
+		vm.Execute(fmt.Sprintf("sudo yum remove -y 'datadog-*'"))
+	} else if _, err = vm.ExecuteWithError("command -v zypper"); err == nil {
+		t.Log("Uninstall with zypper")
+		vm.Execute(fmt.Sprintf("sudo zypper remove -y 'datadog-*'"))
+	} else {
+		require.FailNow(t, "Unknown package manager")
+	}
 }
 
 func (s *installUpdaterTestSuite) assertInstallerInstalled() {
@@ -89,4 +131,34 @@ func (s *installUpdaterTestSuite) assertUninstallInstaller() {
 	t.Log("Assert installer is uninstalled")
 	assertFileNotExists(t, vm, "/opt/datadog-packages/datadog-installer/stable/bin/installer/installer")
 	assertFileNotExists(t, vm, "/opt/datadog-installer/bin/installer/installer")
+}
+
+func (s *installUpdaterTestSuite) assertPackageInstalledByPackageManager(pkg string) {
+	t := s.T()
+	vm := s.Env().VM
+
+	if _, err := vm.ExecuteWithError("command -v apt"); err == nil {
+		vm.Execute("dpkg -l " + pkg + " | grep '^ii'")
+	} else if _, err = vm.ExecuteWithError("command -v yum"); err == nil {
+		vm.Execute("yum list installed " + pkg)
+	} else if _, err = vm.ExecuteWithError("command -v zypper"); err == nil {
+		vm.Execute("zypper se -i " + pkg)
+	} else {
+		require.FailNow(t, "Unknown package manager")
+	}
+}
+
+func (s *installUpdaterTestSuite) assertPackageNotInstalledByPackageManager(pkg string) {
+	t := s.T()
+	vm := s.Env().VM
+
+	if _, err := vm.ExecuteWithError("command -v apt"); err == nil {
+		vm.Execute("! dpkg -l " + pkg + " | grep -q '^ii'")
+	} else if _, err = vm.ExecuteWithError("command -v yum"); err == nil {
+		vm.Execute("! yum list installed " + pkg + " | grep -q " + pkg)
+	} else if _, err = vm.ExecuteWithError("command -v zypper"); err == nil {
+		vm.Execute("! zypper se -i " + pkg + " | grep -q " + pkg)
+	} else {
+		require.FailNow(t, "Unknown package manager")
+	}
 }
