e198a7cfc46555ddebfa7bebb2e7687589036a21
AliDatadog <ali.benabdallah@datadoghq.com>
2024-08-21 16:52:05 +0200

    Apply recommendation

--- test/e2e/common.go
+++ test/e2e/common.go
@@ -261,9 +261,10 @@ func (s *linuxInstallerTestSuite) assertUninstall() {
 		assertFileExists(c, vm, fmt.Sprintf("/etc/%s/%s", s.baseName, s.configFile))
 		if flavor == "datadog-agent" {
 			// The custom file should still be here. All other files, including the extra integration, should be removed
-			assertFileExists(c, vm, fmt.Sprintf("%s/site-packages/testfile", s.getLatestEmbeddedPythonPath("datadog-agent")))
+			expectedFile := fmt.Sprintf("%s/site-packages/testfile", s.getLatestEmbeddedPythonPath("datadog-agent"))
+			assertFileExists(c, vm, expectedFile)
 			files := strings.Split(strings.TrimSuffix(vm.MustExecute("find /opt/datadog-agent -type f"), "\n"), "\n")
-			assert.Len(c, files, 1, fmt.Sprintf("/opt/datadog-agent present after remove, found %v", files))
+			assert.Len(c, files, 1, fmt.Sprintf("/opt/datadog-agent present after remove, found %v, expected only %s", files, expectedFile))
 		} else {
 			// All files in /opt/datadog-agent should be removed
 			assertFileNotExists(c, vm, fmt.Sprintf("/opt/%s", s.baseName))
