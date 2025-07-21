72898f470eef832449a546b0ac5edd6f38f8997e
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2025-07-21 11:04:39 +0200

    fix(debian): Use archive for debian10 (#349)

--- test/vector-test.sh
+++ test/vector-test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+# Patch the sources.list file for debian. This is a workaround, we should change the image instead
+if [[ "${IMAGE}" =~ "debian:10" ]]; then
+  cp ./test/sources.list /etc/apt/sources.list
+fi
+
 # The Vector install script only configures the repos, it doesn't install
 
 $SCRIPT
